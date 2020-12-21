8d3941f90dc31c737229d04297762489a0fc0899
mtedds <github@tedds.org>
2020-12-21 14:14:34 +0000

    Fixed actions to correct Vent-relay sensor

--- insert_actions.sql
+++ insert_actions.sql
@@ -1,7 +1,7 @@
 delete from Action;
 insert into Action (ActionId, SensorName, VariableType, SetValue)
-values (1, "Test-relay", 2, "1")
+values (1, "Vent-relay", 2, "1")
 ;
 insert into Action (ActionId, SensorName, VariableType, SetValue)
-values (2, "Test-relay", 2, "0")
+values (2, "Vent-relay", 2, "0")
 ;
